613f5ef79b1aef7df8d41255a1aa8a8783ed1286
Mauricio Leal <Mauricio.AndresLealVillarael@classmates.com>
2017-04-21 18:48:31 -0500

    Added heroku's database address

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports={
-  "prod" : true ,
-  "heroku" : 'postgres://cnbhxmeyfyjcfp:a4553501856b8d1c75ffcebe71813d7944a450951000001d4338ae18c82996cf@ec2-23-23-186-157.compute-1.amazonaws.com:5432/ddl66s5tol0c9g',
+  "prodConnectionString" : 'postgres://cnbhxmeyfyjcfp:a4553501856b8d1c75ffcebe71813d79postgres://zkdaefmgwacorb:c6d10428c6866d62a83535fe1504310d9128e85f93bab7864494a15749d2331e@ec2-23-23-222-147.compute-1.amazonaws.com:5432/daijm0299g9reb44a450951000001d4338ae18c82996cf@ec2-23-23-186-157.compute-1.amazonaws.com:5432/ddl66s5tol0c9g',
   "connectionString" : 'postgres://localhost:5432/mleal',
   "jwt_secret": "Im Mauricio",
   "key": 'Id0d3v',
