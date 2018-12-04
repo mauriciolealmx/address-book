d06c74d1a5715119c7bc20980f983234f335c323
Mauricio Leal <mauricio.leal@s5stratos.com>
2018-12-04 17:23:16 -0600

    Updated createUser and getUserByEmail postgres querys.

--- src/helpers/postgres-querys.js
+++ src/helpers/postgres-querys.js
@@ -1,62 +1,20 @@
-import Promise from 'bluebird';
-import pg from 'pg';
+import { Client } from 'pg';
 
 import config from '../../config';
 
 const connectionString = process.env.DATABASE_URL || config.connectionString;
+const client = new Client(connectionString);
+client.connect();
 
+// TODO: Need to end() the connection after querys.
 export const getUserByEmail = email => {
-  return new Promise(resolve => {
-    pg.connect(
-      connectionString,
-      (err, client, done) => {
-        // Handle connection errors
-        if (err) {
-          done();
-          return reject(err);
-        }
-        // SQL Query > Find user by email
-        const query = client
-          .query(`SELECT * FROM users WHERE email='${email}'`)
-          .then(({ rows }) => {
-            return resolve(rows);
-          })
-          .catch(err => {
-            return resolve([]);
-          });
-      }
-    );
-  });
+  const queryString = `SELECT * FROM users WHERE email='${email}'`;
+  return client.query(queryString).then(({ rows }) => rows);
 };
 
 export const createUser = (email, encryptedPass) => {
-  return new Promise((resolve, reject) => {
-    const results = [];
-    const data = {
-      email,
-      password: encryptedPass,
-    };
-
-    pg.connect(
-      connectionString,
-      (err, client, done) => {
-        // Handle connection errors
-        if (err) {
-          done();
-          return res.status(500).json({ success: false, data: err });
-        }
-        // SQL Query > Insert Data
-        client.query('INSERT INTO users(email, password) values($1, $2)', [data.email, data.password]);
-        // SQL Query > Find user by email
-        const query = client.query(`SELECT * FROM users WHERE email='${email}'`);
-        query.on('row', row => {
-          results.push(row);
-        });
-        query.on('end', () => {
-          done();
-          return resolve(results);
-        });
-      }
-    );
+  const queryString = 'INSERT INTO users(email, password) values($1, $2)';
+  return client.query(queryString, [email, encryptedPass]).then(() => {
+    return getUserByEmail(email);
   });
 };
