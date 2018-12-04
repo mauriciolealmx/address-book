0563235b8af5d28c5cb41052d92d4f3fe06e2702
Mauricio Leal <mauricio.leal@s5stratos.com>
2018-12-04 17:23:43 -0600

    Updated database creation file.

--- src/models/database.js
+++ src/models/database.js
@@ -1,14 +1,15 @@
-import pg from 'pg';
+import { Client } from 'pg';
+
 import config from '../../config';
 
 const connectionString = process.env.DATABASE_URL || config.connectionString;
-const client = new pg.Client(connectionString);
-
+const client = new Client(connectionString);
 client.connect();
-const query = client.query(
-  'CREATE TABLE users(id SERIAL PRIMARY KEY, email VARCHAR(50) not null, password CHARACTER(32) not null)'
-);
-query.on('end', () => {
+
+const queryString =
+  'CREATE TABLE other(id SERIAL PRIMARY KEY, email VARCHAR(50) not null, password CHARACTER(32) not null)';
+
+client.query(queryString).then(() => {
   console.log('Table created');
   client.end();
 });
