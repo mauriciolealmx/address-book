c75d93657e3df17a7d36fad5580fe855ceae3156
Mauricio Leal <mauricio.leal@s5stratos.com>
2019-02-08 17:02:56 -0600

    Added pending TODO.

--- src/routes/removeUser.js
+++ src/routes/removeUser.js
@@ -2,6 +2,7 @@ import pgClient from '../helpers/postgres-querys';
 
 export const removeUser = (req, res) => {
   pgClient.deleteUser(req.body.email, (err, data) => {
+    // TODO: User needs to be be deleted from Firebase as well.
     let message;
     if (data) {
       message = `User with email: ${req.body.email} was deleted`;
